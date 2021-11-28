14c8feec942fc578145d5c096f38dc76b2af49ef
Jules <jfrancois968@gmail.com>
2021-11-28 22:46:57 +0100

    Revert ":poop: Test node x mysql ECHEC"
    This reverts commit c21a2578b1d2b9c14ac0fedd7029d3a8ff390050.

--- back/js/database/entity/index.js
+++
@@ -1,2 +0,0 @@
-export * from './User.js';
-//# sourceMappingURL=index.js.map
--- back/types/database/entity/User.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    id: number;
-    pseudo: string;
-    firstname: string;
-    lastname: string;
-    password: string;
-    email: string;
-    constructor(pseudo: string, firstname: string, lastname: string, password: string, email: string);
-}
