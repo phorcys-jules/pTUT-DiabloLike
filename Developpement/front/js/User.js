702d169c6dd335094f549a89dcdd63cec2b35781
Wilt Lilian <lilian.wilt@hotmail.com>
2022-01-03 13:01:39 +0100

    modif css + maj code effacé

--- Developpement/front/js/User.js
+++ Developpement/front/js/User.js
@@ -1,7 +1,15 @@
-class User {
-    constructor(name, password, chars) {
-        this.name = name;
+export class User {
+    constructor(pseudo, firstname, lastname, password, email) {
+
+        this.pseudo = pseudo;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.password = password;
+        this.email = email;
+    }
+
+    toString() {
+        return `Nom : ${this.lastname} / Prenom : ${this.firstname} / Pseudo : ${this.pseudo} a ete créé`;
     }
 }
-export {};
+
