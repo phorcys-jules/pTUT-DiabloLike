b64cfba5a345a7f238c633427d3405c56e5417bb
Jules <jfrancois968@gmail.com>
2022-01-05 18:20:25 +0100

    🔥 🎨 Suppresion fichier js , inutile puisqu'ils sont obtenu à la transpilation.

--- Developpement/front/js/main.js
+++
@@ -1,6 +0,0 @@
-var _a;
-import { Guerrier } from "./character/Guerrier.js";
-import { bootstrap } from "./engine/bootstrap.js";
-(_a = document.getElementById('run')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-    bootstrap(new Guerrier('Conan'));
-});
